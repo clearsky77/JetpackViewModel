6e33fa7358fd487c0f8f16235172625143242838
clearsky77 <clearsky3264@gmail.com>
2022-01-23 20:45:51 +0900

    ViewModel 작성 2.함수 작성  - enum class 활용

--- app/src/main/java/com/clearsky77/jetpackviewmodel/MyNumberViewModel.kt
+++ app/src/main/java/com/clearsky77/jetpackviewmodel/MyNumberViewModel.kt
@@ -4,6 +4,10 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+enum class ActionType{
+    PLUS, MINUS
+}
+
 class MyNumberViewModel : ViewModel() {
 
     companion object {
@@ -20,4 +24,12 @@ class MyNumberViewModel : ViewModel() {
         _currentValue.value = 0; // Mutable이라서 변경이 가능하다.
     }
 
+    fun updateValue(actionType: ActionType, input: Int){
+        when(actionType){
+            ActionType.PLUS ->
+                _currentValue.value = _currentValue.value?.plus(input)
+            ActionType.MINUS ->
+                _currentValue.value = _currentValue.value?.minus(input)
+        }
+    }
 }
