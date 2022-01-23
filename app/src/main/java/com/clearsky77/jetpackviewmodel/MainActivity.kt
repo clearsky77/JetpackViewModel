855a8533b199e529bce5e301a98bc34909f75173
clearsky77 <clearsky3264@gmail.com>
2022-01-23 21:10:39 +0900

    ViewModel 생성. 값을 변경하는 코드 작성.

--- app/src/main/java/com/clearsky77/jetpackviewmodel/MainActivity.kt
+++ app/src/main/java/com/clearsky77/jetpackviewmodel/MainActivity.kt
@@ -2,10 +2,28 @@ package com.clearsky77.jetpackviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myNumberViewModel: MyNumberViewModel
+
+    companion object {
+        private val TAG: String = "태그"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myNumberViewModel = ViewModelProvider(this).get(myNumberViewModel::class.java)
+        myNumberViewModel.currentValue.observe(this, Observer {
+            Log.d(TAG, "MainActivity - currentValue: $it")
+            txtNum.text = it.toString()
+        })
+
     }
 }
